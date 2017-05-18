7007aeb6be051589b69aea9d32ad53c94aca2846
shijin <shijin@feezu.cn>
2017-05-18 14:26:19 +0800

    Add a put and get test method

--- concurrency/src/test/java/concurrency/TestBufferQueue.java
+++ concurrency/src/test/java/concurrency/TestBufferQueue.java
@@ -1,6 +1,7 @@
 package concurrency;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Rule;
@@ -29,6 +30,27 @@ public class TestBufferQueue {
     assertThat(queue.toString(), containsString("Test1"));
   }
 
+  @Test
+  public void testPutAndGet() {
+    BufferQueue queue = new BufferQueue();
+    String[] talks = {"Hello", "World", "!"};
+    
+    try {
+      queue.put(talks[0]);
+      queue.put(talks[1]);
+      queue.put(talks[2]);
+      
+      assertThat(queue.take(), equalTo(talks[0]));
+      assertThat(queue.take(), equalTo(talks[1]));
+      assertThat(queue.take(), equalTo(talks[2]));
+      
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    
+    
+  }
+  
   @Test(timeout = 1500)
   public void testLongTime() {
     for (int i = 0; i < 10; i++) {
@@ -41,11 +63,12 @@ public class TestBufferQueue {
     }
   }
 
+  @SuppressWarnings("null")
   @Test
   public void testException() {
     thrown.expect(NullPointerException.class);
     String s = null;
-
+    
     if (s.equals("S")) {
       
     }
