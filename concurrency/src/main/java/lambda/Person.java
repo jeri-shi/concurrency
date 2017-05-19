76d80a832bc8438eb8291e99243d421ddd6c87f4
shijin <shijin@feezu.cn>
2017-05-19 17:21:48 +0800

    add Person implementation

--- concurrency/src/main/java/lambda/Person.java
+++ concurrency/src/main/java/lambda/Person.java
@@ -1,27 +1,49 @@
 package lambda;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
-  
+
   public enum Sex {
     MALE, FEMALE
   }
-  
-  String name;
-  LocalDate birthday;
-  Sex gendar;
-  String emailAddress;
-  
-  public int getAge() {
-    return 0;
+
+  private String name;
+  private LocalDate birthday;
+  private Sex gendar;
+  private String emailAddress;
+
+  public Person(String name, LocalDate birthday, Sex gendar, String email) {
+    this.name = name;
+    this.birthday = birthday;
+    this.gendar = gendar;
+    this.emailAddress = email;
   }
-  
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
   public Sex getGendar() {
-    return Sex.MALE;
+    return gendar;
   }
-  public void printPerson() {
-    
+
+  public int getAge() {
+    LocalDate current = LocalDate.now();
+    Period period = Period.between(birthday, current);
+    return period.getYears();
+//    Duration duration = Duration.between(birthday, current);
+//    return (int) duration.toDays() / 365;
+  }
+
+  public String printPerson() {
+    return this.toString();
   }
 
 }
